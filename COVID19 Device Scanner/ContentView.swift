d29711f1687ca1a5dd64eab6314dcf10483ccbbc
TETRA2000 <sagittariusm25@gmail.com>
2020-07-02 08:50:13 +0900

    Initial Commit

---
+++ COVID19 Device Scanner/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  COVID19 Device Scanner
+//
+//  Created by Takahiko Inayama on 2020/07/02.
+//  Copyright © 2020 TETRA2000. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
